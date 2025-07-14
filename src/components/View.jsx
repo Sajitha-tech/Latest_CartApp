8e07601c4bf206b6341d7fd5f71130fe840116e7
Sajitha-tech <sajithats1234@gmail.com>
2025-07-14 19:44:41 +0530

    Initial commit

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -9,14 +9,10 @@ const View = () => {
             .then((response) => {
                 changeCart(response.data);
             })
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-            });
+            .catch()
     }
 
-    useEffect(() => {
-        fetchdata();
-    }, []);
+    useEffect(() => { fetchdata()   }, []);
 
     return (
         <div>
@@ -27,10 +23,13 @@ const View = () => {
                             return (
                                 <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3" key={y}>
                                     <div className="card mb-4">
+                                          <img src={x.images} alt="" className="image" />
                                         <div className="card-body">
+                                          
                                             <h5 className="card-title">{x.title}</h5>
                                             <h6 className="card-subtitle mb-2 text-muted">{x.category}</h6>
                                             <p className="card-text">{x.description}</p>
+                                            
                                             <p className="card-text">Discount: {x.discountPercentage}%</p>
                                             <p className="card-text">Price: ${x.price}</p>
                                             <a href="#" className="card-link">Visit</a>
